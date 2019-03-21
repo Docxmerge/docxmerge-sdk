f52190c2d783164f94cbc1c817c82601e2b68f19
dviejo <dvp123456@gmail.com>
2019-03-21 11:45:10 +0100

    update nodejs

--- nodejs/src/index.test.ts
+++ nodejs/src/index.test.ts
@@ -1,97 +1,153 @@
-import test = require("tape");
-import fs = require("fs");
-import Docxmerge = require("./");
-import path = require("path");
-import { isBuffer } from "util";
-import { Stream } from "stream";
-const fixturesDir = path.join(__dirname, "fixtures");
-const tmpDir = path.join(process.cwd(), "tmp");
+import test = require("tape")
+import fs = require("fs")
+import Docxmerge = require("./")
+import path = require("path")
+import { isBuffer } from "util"
+import { Stream } from "stream"
+const fixturesDir = path.join(__dirname, "fixtures")
+const tmpDir = path.join(process.cwd(), "tmp")
 if (!fs.existsSync(tmpDir)) {
-  fs.mkdirSync(tmpDir);
+  fs.mkdirSync(tmpDir)
 }
-const apiKey = "8f22c304d0b44af2b03c2ad3d1aa5471";
-const tenantId = "f60b0638-2f2e-4f92-b027-39cedfa06d4d";
-const getDocxmerge = () => new Docxmerge(apiKey, "http://localhost:5030");
+const apiKey = "580c2336ca7944a48626e70eeb243083"
+const tenantId = "default"
+const getDocxmerge = () => new Docxmerge(apiKey, "http://localhost:5030")
 const getHelloworldTmpl = () =>
-  fs.readFileSync(path.join(fixturesDir, "helloworld.docx"));
+  fs.readFileSync(path.join(fixturesDir, "helloworld.docx"))
 const saveFile = (data: Buffer, filename: string) =>
-  fs.writeFileSync(path.join(tmpDir, filename), data);
+  fs.writeFileSync(path.join(tmpDir, filename), data)
 const saveStream = (data: Stream, filename: string) =>
-  data.pipe(fs.createWriteStream(path.join(tmpDir, filename)));
-const templateName = "Hello world";
+  data.pipe(fs.createWriteStream(path.join(tmpDir, filename)))
+const templateName = "Hello world"
+test("Render with tags", async t => {
+  const docxmerge = getDocxmerge()
+  const pdf = await docxmerge.convertTemplate(
+    "default",
+    "Hello world",
+    {
+      pec: "",
+    },
+    "DRAFT",
+    1,
+  )
+  t.true(pdf)
+  t.assert(isBuffer(pdf))
+  saveFile(pdf, "pec.pdf")
+  t.end()
+})
+test.only("Render url", async t => {
+  const docxmerge = getDocxmerge()
+  const pdf = await docxmerge.renderUrl(
+    "landing",
+    "https://minio.nextagilesoft.com/public/InvoiceTemplate.docx",
+    {
+      company: "Your Company Name",
+      invid: "INVOICE",
+      date: "01-01-2013",
+      address: "Address",
+      city: "City",
+      state: "State",
+      zip: "Zip",
+      client_name: "Client Name",
+      balance: "$0.00",
+      notes: "Use this space for comments to your client.",
+      orders: [
+        {
+          description: "Description",
+          qty: "4",
+          price: "9.99",
+          total: "40",
+        },
+        {
+          description: "Description",
+          qty: "4",
+          price: "9.99",
+          total: "40",
+        },
+      ],
+      subtotal: "$0.00",
+      tax: "$0.00",
+      total: "$0.00",
+    },
+  )
+  t.true(pdf)
+  t.assert(isBuffer(pdf))
+  saveFile(pdf, "curr.pdf")
+  t.end()
+})
 test("Merge template", async t => {
-  const docxmerge = getDocxmerge();
+  const docxmerge = getDocxmerge()
   const docx = await docxmerge.mergeTemplate(
     tenantId,
     templateName,
     {
-      hello_world: "Hello world.."
+      hello_world: "Hello world..",
     },
     {},
     "DRAFT",
-    1
-  );
-  t.true(docx);
-  t.assert(isBuffer(docx));
-  saveFile(docx, "helloworld_merged1.docx");
-  t.end();
-});
+    1,
+  )
+  t.true(docx)
+  t.assert(isBuffer(docx))
+  saveFile(docx, "helloworld_merged1.docx")
+  t.end()
+})
 
 test("Merge file", async t => {
-  const docxmerge = getDocxmerge();
+  const docxmerge = getDocxmerge()
   const docx = await docxmerge.mergeFile(tenantId, getHelloworldTmpl(), {
-    hello_world: "Hello world."
-  });
-  t.true(docx);
-  t.assert(isBuffer(docx));
-  saveFile(docx, "helloworld_merged.docx");
-  t.end();
-});
+    hello_world: "Hello world.",
+  })
+  t.true(docx)
+  t.assert(isBuffer(docx))
+  saveFile(docx, "helloworld_merged.docx")
+  t.end()
+})
 
 test("Render file", async t => {
-  const docxmerge = getDocxmerge();
+  const docxmerge = getDocxmerge()
   const pdf = await docxmerge.renderFile(tenantId, getHelloworldTmpl(), {
-    hello_world: "Hello world."
-  });
-  saveFile(pdf, "helloworld_merged2.pdf");
-  t.end();
-});
+    hello_world: "Hello world.",
+  })
+  saveFile(pdf, "helloworld_merged2.pdf")
+  t.end()
+})
 
 test("Render template", async t => {
-  const docxmerge = getDocxmerge();
+  const docxmerge = getDocxmerge()
   const { content } = await docxmerge.renderTemplate(
     tenantId,
     templateName,
     {
-      hello_world: "Hello world."
+      hello_world: "Hello world.",
     },
     {},
     "DRAFT",
-    1
-  );
-  t.true(content);
-  saveStream(content, "helloworld_merged.pdf");
-  t.end();
-});
+    1,
+  )
+  t.true(content)
+  saveStream(content, "helloworld_merged.pdf")
+  t.end()
+})
 test("Convert template", async t => {
-  const docxmerge = getDocxmerge();
+  const docxmerge = getDocxmerge()
   const pdf = await docxmerge.convertTemplate(
     tenantId,
     templateName,
     {},
     "DRAFT",
-    1
-  );
-  saveFile(pdf, "helloworld.pdf");
-  t.true(pdf);
-  t.assert(isBuffer(pdf));
-  t.end();
-});
+    1,
+  )
+  saveFile(pdf, "helloworld.pdf")
+  t.true(pdf)
+  t.assert(isBuffer(pdf))
+  t.end()
+})
 test("Convert file", async t => {
-  const docxmerge = getDocxmerge();
-  const pdf = await docxmerge.convertFile(tenantId, getHelloworldTmpl());
-  saveFile(pdf, "helloworld.pdf");
-  t.true(pdf);
-  t.assert(isBuffer(pdf));
-  t.end();
-});
+  const docxmerge = getDocxmerge()
+  const pdf = await docxmerge.convertFile(tenantId, getHelloworldTmpl())
+  saveFile(pdf, "helloworld.pdf")
+  t.true(pdf)
+  t.assert(isBuffer(pdf))
+  t.end()
+})
--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,50 +1,50 @@
-import qs = require("querystring");
-import request = require("request");
-import { Stream } from "stream";
+import qs = require("querystring")
+import request = require("request")
+import { Stream } from "stream"
 
-import { ApiApi, ApiKeyAuth, TemplatesApi } from "./swagger/api";
-type EnvType = "DRAFT" | "TESTING" | "PRODUCTION";
+import { ApiApi, ApiKeyAuth, TemplatesApi } from "./swagger/api"
+type EnvType = "DRAFT" | "TESTING" | "PRODUCTION"
 interface KeyValue {
-  [key: string]: string;
+  [key: string]: string
 }
 interface ReportResponse {
-  url: string;
-  reportId: string;
-  content: Stream;
+  url: string
+  reportId: string
+  content: Stream
 }
 const wordContentType =
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-const jsonContentType = "application/json";
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+const jsonContentType = "application/json"
 class Docxmerge {
-  private templatesApi: TemplatesApi;
-  private apiApi: ApiApi;
+  private templatesApi: TemplatesApi
+  private apiApi: ApiApi
 
   constructor(private apiKey: string, private readonly basePath) {
-    this.basePath = this.basePath || "https://api.docxmerge.com";
-    const auth = new ApiKeyAuth("header", "ApiKey");
-    auth.apiKey = apiKey;
+    this.basePath = this.basePath || "https://api.docxmerge.com"
+    const auth = new ApiKeyAuth("header", "ApiKey")
+    auth.apiKey = apiKey
 
-    const templatesApi = new TemplatesApi(this.basePath);
-    const apiApi = new ApiApi(this.basePath);
+    const templatesApi = new TemplatesApi(this.basePath)
+    const apiApi = new ApiApi(this.basePath)
 
-    templatesApi.setDefaultAuthentication(auth);
-    apiApi.setDefaultAuthentication(auth);
-    this.templatesApi = templatesApi;
-    this.apiApi = apiApi;
+    templatesApi.setDefaultAuthentication(auth)
+    apiApi.setDefaultAuthentication(auth)
+    this.templatesApi = templatesApi
+    this.apiApi = apiApi
   }
   getTemplates(tenant: string, page: number, size: number) {
-    return this.templatesApi.apiByTenantTemplatesGet(tenant, size, page);
+    return this.templatesApi.apiByTenantTemplatesGet(tenant, size, page)
   }
   getReportById(tenant: string, id: string) {
-    return this.templatesApi.apiByTenantReportsByIdGet(id, tenant);
+    return this.templatesApi.apiByTenantReportsByIdGet(id, tenant)
   }
   async renderFile(tenantId: string, file: Buffer, data: KeyValue) {
     const { body } = await this.templatesApi.apiByTenantPrintPost(
       tenantId,
       this.getDocxFormFile(file),
-      this.getJsonFormFile(data)
-    );
-    return body;
+      this.getJsonFormFile(data),
+    )
+    return body
   }
   renderTemplate(
     tenant: string,
@@ -52,14 +52,14 @@ class Docxmerge {
     data: object,
     attributes: KeyValue = {},
     env?: EnvType,
-    version?: number
+    version?: number,
   ): Promise<ReportResponse> {
     return new Promise<ReportResponse>((resolve, reject) => {
       const query = {
         version,
         env,
-        ...attributes
-      };
+        ...attributes,
+      }
 
       this.getRequestApi().post(
         `/${tenant}/templates/${templateName}/render?${qs.stringify(query)}`,
@@ -67,51 +67,98 @@ class Docxmerge {
           json: true,
           body: data,
           headers: {
-            ApiKey: this.apiKey
-          }
+            ApiKey: this.apiKey,
+          },
         },
         async (err, res) => {
           if (err) {
-            return reject(err);
+            return reject(err)
           }
           if (res.statusCode !== 201) {
-            return reject(new Error(`Bad status code${res.statusCode}`));
+            return reject(new Error(`Bad status code${res.statusCode}`))
           }
-          const url = res.headers.location;
+          const url = res.headers.location
           resolve({
             reportId: res.body.id,
             url,
-            content: this.getRequestApi().get(url, { baseUrl: "" })
-          });
-        }
-      );
-    });
+            content: this.getRequestApi().get(url, { baseUrl: "" }),
+          })
+        },
+      )
+    })
+  }
+  async renderUrl(tenant: string, url: string, data: any) {
+    return new Promise<Buffer>((resolve, reject) => {
+      this.getRequestApi().post(
+        `/${tenant}/print`,
+        {
+          headers: {
+            ApiKey: this.apiKey,
+          },
+          encoding: null,
+          formData: {
+            document: {
+              value: request(url),
+              options: {
+                filename: "file.docx",
+                contentType: wordContentType,
+              },
+            },
+            data: {
+              value: JSON.stringify(data),
+              options: {
+                filename: "file.json",
+                contentType: "application/json",
+              },
+            },
+          },
+        },
+        (err, res, body) => {
+          if (err) return reject(err)
+          resolve(res.body)
+        },
+      )
+    })
   }
   async convertTemplate(
     tenant: string,
     templateName: string,
     attributes: KeyValue = {},
     env?: EnvType,
-    version?: number
+    version?: number,
   ) {
-    const {
-      body
-    } = await this.templatesApi.apiByTenantTemplatesByTemplateNameConvertPost(
-      templateName,
-      tenant,
-      version,
-      false,
-      attributes,
-      env
-    );
-    return body;
+    return new Promise<Buffer>((resolve, reject) => {
+      const query = {
+        version,
+        env,
+        ...attributes,
+      }
+
+      this.getRequestApi().post(
+        `/${tenant}/templates/${templateName}/convert?${qs.stringify(query)}`,
+        {
+          json: true,
+          body: {},
+          encoding: null,
+          headers: {
+            ApiKey: this.apiKey,
+          },
+        },
+        async (err, res, body) => {
+          if (err) {
+            return reject(err)
+          }
+          resolve(res.body)
+        },
+      )
+    })
   }
   async convertFile(tenant: string, file: Buffer) {
     const { body, response } = await this.templatesApi.apiByTenantConvertPost(
       tenant,
-      this.getDocxFormFile(file)
-    );
-    return body;
+      this.getDocxFormFile(file),
+    )
+    return body
   }
   async mergeTemplate(
     tenant: string,
@@ -119,14 +166,14 @@ class Docxmerge {
     data: KeyValue,
     attributes: KeyValue = {},
     env?: EnvType,
-    version?: number
+    version?: number,
   ) {
     return new Promise<Buffer>((resolve, reject) => {
       const query = {
         version,
         env,
-        ...attributes
-      };
+        ...attributes,
+      }
 
       this.getRequestApi().post(
         `/${tenant}/templates/${templateName}/merge?${qs.stringify(query)}`,
@@ -135,55 +182,55 @@ class Docxmerge {
           body: data,
           encoding: null,
           headers: {
-            ApiKey: this.apiKey
-          }
+            ApiKey: this.apiKey,
+          },
         },
         async (err, res, body) => {
           if (err) {
-            return reject(err);
+            return reject(err)
           }
-          resolve(res.body);
-        }
-      );
-    });
+          resolve(res.body)
+        },
+      )
+    })
   }
   async mergeFile(tenant: string, file: Buffer, data: KeyValue = {}) {
     const { body } = await this.templatesApi.apiByTenantMergePost(
       tenant,
       this.getDocxFormFile(file),
-      this.getJsonFormFile(data)
-    );
-    return body;
+      this.getJsonFormFile(data),
+    )
+    return body
   }
   private getDocxFormFile(file: Buffer) {
-    return this.getFormFile(file, "file.docx", wordContentType);
+    return this.getFormFile(file, "file.docx", wordContentType)
   }
   private getJsonFormFile(data: KeyValue) {
     return this.getFormFile(
       Buffer.from(JSON.stringify(data)),
       "file.json",
-      jsonContentType
-    );
+      jsonContentType,
+    )
   }
   private getFormFile(
     file: Buffer,
     filename: string = "file",
-    contentType: string = "application/octet-stream"
+    contentType: string = "application/octet-stream",
   ): any {
     return {
       value: file,
       options: {
         filename,
-        contentType
-      }
-    };
+        contentType,
+      },
+    }
   }
   private getRequestApi() {
     return request.defaults({
       json: true,
       headers: { ApiKey: this.apiKey },
-      baseUrl: `${this.basePath}/api`
-    });
+      baseUrl: `${this.basePath}/api`,
+    })
   }
 }
-export = Docxmerge;
+export = Docxmerge
